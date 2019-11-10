f01f02e62a87200b54367ace578c7dfc820b5904
ntaek <ntaek1026@gmail.com>
2019-11-10 11:44:10 +0900

    python

---
+++ sparta-python/music.py
@@ -0,0 +1,33 @@
+import requests
+from bs4 import BeautifulSoup
+from pymongo import MongoClient
+
+client = MongoClient('localhost', 27017)
+db = client.dbsparta
+
+headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+data = requests.get('https://www.genie.co.kr/chart/top200?ditc=D&rtm=N&ymd=20190908',headers=headers)
+
+soup = BeautifulSoup(data.text, 'html.parser')
+
+genie = soup.select('#body-content > div > div > table > tbody > tr')
+
+
+rank = 1
+for music in genie:
+    a_tag = music.select_one('td.info > a.title.ellipsis')
+    b_tag = music.select_one('td.info > a.artist.ellipsis')
+    #b_tag = movie.select_one('td.point')
+    if not a_tag == None:
+        #rank = b_tag.text
+        title = a_tag.text
+        singer = b_tag.text
+        print(rank, title.strip(), singer.strip())
+
+        doc = {
+            'rank' : rank,
+            'title' : title,
+            'singer' : singer
+        }
+        db.musics.insert_one(doc)
+        rank += 1
